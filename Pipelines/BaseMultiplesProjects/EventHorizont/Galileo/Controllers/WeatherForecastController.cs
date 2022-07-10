564897910f933fbc82f2ab8b833a06652e521889
Wister <e@wister.mx>
2022-07-09 23:59:50 -0500

    error

--- Pipelines/BaseMultiplesProjects/EventHorizont/Galileo/Controllers/WeatherForecastController.cs
+++ Pipelines/BaseMultiplesProjects/EventHorizont/Galileo/Controllers/WeatherForecastController.cs
@@ -26,7 +26,7 @@ public class WeatherForecastController : ControllerBase
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
+        }
         .ToArray();
     }
 }
